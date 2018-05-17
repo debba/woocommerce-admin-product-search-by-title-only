5fd4b732ae60234e011f56f07e6a81e54a38fed2
debba <andrea@debbaweb.it>
2018-05-17 18:10:57 +0200

    - year correction

--- plugin.php
+++ plugin.php
@@ -15,7 +15,7 @@
  * GitHub Plugin URI: https://github.com/debba/woocommerce-admin-product-search-by-title-only
  */
 /**
-Copyright 2016 Andrea Debernardi (email : andrea.debernardi@dueclic.com)
+Copyright 2018 Andrea Debernardi (email : andrea.debernardi@dueclic.com)
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License, version 2, as
 published by the Free Software Foundation.
